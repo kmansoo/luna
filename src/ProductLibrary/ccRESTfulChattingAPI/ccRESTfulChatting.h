f8615d1133cf4a0894cd686a1d47d27ececb8a49
Mansoo <mansoo.kim@icloud.com>
2015-11-30 20:36:18 +0900

    implemented the '/user' Web API

--- src/ProductLibrary/ccRESTfulChattingAPI/ccRESTfulChatting.h
+++ src/ProductLibrary/ccRESTfulChattingAPI/ccRESTfulChatting.h
@@ -12,6 +12,70 @@
 
 using namespace std;
 
+class ccRESTfulChattingUserInfo
+{
+public:
+    std::string     _strID;
+    std::string     _strName;
+};
+
+class ccRESTfulChattingMessageInfo
+{
+public:
+    std::string     _strDateTime;
+    std::string     _strFromID;
+    std::string     _strMessage;
+};
+
+class ccRESTfulChattingSessionInfo
+{
+public:
+    ccRESTfulChattingSessionInfo(const std::string& strSessionName, const std::string& strOwnerID);
+    ~ccRESTfulChattingSessionInfo() {}
+
+public:
+    bool    Join(const std::string& strUserID);
+    bool    Leave(const std::string& strUserID);
+    bool    NewMessage(const std::string& strUserID, const std::string& strMessage);
+
+public:
+    std::string     _strName;
+    std::string     _strOwnerID;
+
+private:
+    std::vector<std::string>    _aMembers;
+    std::vector<std::shared_ptr<ccRESTfulChattingMessageInfo>>  _aMessages;
+};
+
+class ccRESTfulChattingSessionManager
+{
+public:
+    ccRESTfulChattingSessionManager() {}
+
+public:
+    bool    CreateID(const std::string& strID, const std::string& strName);
+    bool    DeleteID(const std::string& strID);
+
+    bool    CreateSession(const std::string& strName, const std::string& strOwnerID);
+    bool    DeleteSession(const std::string& strName, const std::string& strRequestID);
+
+    bool    JoinSession(const std::string& strSessionName, const std::string& strUserID);
+    bool    LeaveSession(const std::string& strSessionName, const std::string& strUserID);
+
+    bool    GetUserList(std::vector<std::string>& aUserIDs);
+    bool    GetSessionList(std::vector<std::string>& aSessionNames);
+    bool    GetSessionInfo(const std::string& strSessionName, std::vector<std::string>& aMembers);
+
+    bool    BroadcastMessage(const std::string& strSessionName, const std::string& strUserID, const std::string& strMessage);
+
+protected:
+    std::shared_ptr<ccRESTfulChattingSessionInfo> DoFindSession(const std::string& strName);
+
+private:
+    std::vector< std::shared_ptr<ccRESTfulChattingUserInfo>>    _aUserInfos;
+    std::vector< std::shared_ptr<ccRESTfulChattingSessionInfo>> _aSessionInfos;
+};
+
 class ccRESTfulChatting : public ccRESTfulApi
 {
 public:
@@ -19,10 +83,13 @@ public:
     virtual ~ccRESTfulChatting();
 
 protected:
-    bool    list(std::shared_ptr<ccWebServerRequest> pRequest, std::shared_ptr<ccWebServerResponse> pResponse);
+    bool    user(std::shared_ptr<ccWebServerRequest> pRequest, std::shared_ptr<ccWebServerResponse> pResponse);
     bool    session(std::shared_ptr<ccWebServerRequest> pRequest, std::shared_ptr<ccWebServerResponse> pResponse);
     bool    session_member(std::shared_ptr<ccWebServerRequest> pRequest, std::shared_ptr<ccWebServerResponse> pResponse);
     bool    session_message(std::shared_ptr<ccWebServerRequest> pRequest, std::shared_ptr<ccWebServerResponse> pResponse);
+
+private:
+    ccRESTfulChattingSessionManager   _oSessionManager;
 };
 
 
--- src/ProductLibrary/ccRESTfulChattingAPI/src/ccRESTfulChatting.cpp
+++ src/ProductLibrary/ccRESTfulChattingAPI/src/ccRESTfulChatting.cpp
@@ -8,18 +8,196 @@
 #include <iostream>
 #include <functional>
 
+#include <iomanip>
+#include <ctime>
+
+#include <sstream>
+
+#include "ccCoreAPI/ccString.h"
+
 #include "ccJsonParserAPI/json/value.h"
 #include "ccJsonParserAPI/json/writer.h"
 
 #include "../ccRESTfulChatting.h"
 
+///
+ccRESTfulChattingSessionInfo::ccRESTfulChattingSessionInfo(const std::string& strSessionName, const std::string& strOwnerID) : 
+    _strName(strSessionName),
+    _strOwnerID(strOwnerID)
+{
+    _aMembers.push_back(strOwnerID);
+}
+
+bool ccRESTfulChattingSessionInfo::Join(const std::string& strUserID)
+{
+    for (auto user : _aMembers)
+    {
+        if (user == strUserID)
+            return false;
+    }
+
+    _aMembers.push_back(strUserID);
+
+    return true;
+}
+
+bool ccRESTfulChattingSessionInfo::Leave(const std::string& strUserID)
+{
+    std::vector<std::string>::iterator  it;
+
+    for (it = _aMembers.begin(); it != _aMembers.end(); it++)
+    {
+        if (*it == strUserID)
+        {
+            _aMembers.erase(it);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool ccRESTfulChattingSessionInfo::NewMessage(const std::string& strUserID, const std::string& strMessage)
+{
+    std::shared_ptr<ccRESTfulChattingMessageInfo> pNewMessage(new ccRESTfulChattingMessageInfo);
+
+    auto t  = std::time(nullptr);
+    auto tm = *std::localtime(&t);
+
+    std::stringstream ss;
+    
+    ss << std::put_time(&tm, "%d-%m-%Y %H-%M-%S");
+
+    pNewMessage->_strDateTime   = ss.str();
+    pNewMessage->_strFromID     = strUserID;
+    pNewMessage->_strMessage    = strMessage;
+
+    return true;
+}
+
+//--
+//  ccRESTfulChattingSessionInfo
+//
+bool ccRESTfulChattingSessionManager::CreateID(const std::string& strID, const std::string& strName)
+{
+    for (auto userInfo : _aUserInfos)
+    {
+        if (userInfo->_strID == strID)
+            return false;
+    }
+
+    std::shared_ptr<ccRESTfulChattingUserInfo> pNewUser(new ccRESTfulChattingUserInfo);
+
+    pNewUser->_strID = strID;
+    pNewUser->_strName = strName;
+
+    _aUserInfos.push_back(pNewUser);
+
+    return true;
+}
+
+bool ccRESTfulChattingSessionManager::DeleteID(const std::string& strID)
+{
+    std::vector<std::shared_ptr<ccRESTfulChattingUserInfo>>::iterator  it;
+
+    for (it = _aUserInfos.begin(); it != _aUserInfos.end(); it++)
+    {
+        if ((*it)->_strID == strID)
+        {
+            _aUserInfos.erase(it);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool ccRESTfulChattingSessionManager::CreateSession(const std::string& strName, const std::string& strOwnerID)
+{
+    for (auto sessionInfo : _aSessionInfos)
+    {
+        if (sessionInfo->_strName == strName)
+            return false;
+    }
+
+    std::shared_ptr<ccRESTfulChattingSessionInfo> pNewSession(new ccRESTfulChattingSessionInfo(strName, strOwnerID));
+
+    _aSessionInfos.push_back(pNewSession);
+
+    return true;
+}
+
+bool ccRESTfulChattingSessionManager::DeleteSession(const std::string& strName, const std::string& strOwnerID)
+{
+    std::vector<std::shared_ptr<ccRESTfulChattingSessionInfo>>::iterator  it;
+
+    for (it = _aSessionInfos.begin(); it != _aSessionInfos.end(); it++)
+    {
+        if ((*it)->_strName == strName && (*it)->_strOwnerID == strOwnerID)
+        {
+            _aSessionInfos.erase(it);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool ccRESTfulChattingSessionManager::JoinSession(const std::string& strName, const std::string& strOwnerID)
+{
+
+
+    return false;
+
+}
+
+bool ccRESTfulChattingSessionManager::LeaveSession(const std::string& strName, const std::string& strOwnerID)
+{
+
+    return false;
+}
+
+bool ccRESTfulChattingSessionManager::GetUserList(std::vector<std::string>& aUserIDs)
+{
+    aUserIDs.clear();
+
+    for (auto info : _aUserInfos)
+        aUserIDs.push_back(info->_strID);
+
+    return true;
+}
+
+bool ccRESTfulChattingSessionManager::GetSessionList(std::vector<std::string>& aSessionNames)
+{
+    aSessionNames.clear();
+
+    for (auto info : _aSessionInfos)
+        aSessionNames.push_back(info->_strName);
+
+    return true;
+}
+
+bool ccRESTfulChattingSessionManager::GetSessionInfo(const std::string& strSessionName, std::vector<std::string>& aMembers)
+{
+
+    return false;
+}
+
+bool  ccRESTfulChattingSessionManager::BroadcastMessage(const std::string& strSessionName, const std::string& strUserID, const std::string& strMessage)
+{
+
+    return false;
+}
+
+
+/////
 ccRESTfulChatting::ccRESTfulChatting()
 {
     // TODO Auto-generated constructor stub
-    AddAPI(std::string("/list"), std::bind(&ccRESTfulChatting::list, this, std::placeholders::_1, std::placeholders::_2));
+    AddAPI(std::string("/user"), std::bind(&ccRESTfulChatting::user, this, std::placeholders::_1, std::placeholders::_2));
     AddAPI(std::string("/session"), std::bind(&ccRESTfulChatting::session, this, std::placeholders::_1, std::placeholders::_2));
-    AddAPI(std::string("/session_member"), std::bind(&ccRESTfulChatting::session_member, this, std::placeholders::_1, std::placeholders::_2));
-    AddAPI(std::string("/session_message"), std::bind(&ccRESTfulChatting::session_message, this, std::placeholders::_1, std::placeholders::_2));
+    AddAPI(std::string("/session/member"), std::bind(&ccRESTfulChatting::session_member, this, std::placeholders::_1, std::placeholders::_2));
+    AddAPI(std::string("/session/message"), std::bind(&ccRESTfulChatting::session_message, this, std::placeholders::_1, std::placeholders::_2));
 }
 
 ccRESTfulChatting::~ccRESTfulChatting()
@@ -27,14 +205,37 @@ ccRESTfulChatting::~ccRESTfulChatting()
     // TODO Auto-generated destructor stub
 }
 
-bool ccRESTfulChatting::list(std::shared_ptr<ccWebServerRequest> pRequest, std::shared_ptr<ccWebServerResponse> pResponse)
+bool ccRESTfulChatting::user(std::shared_ptr<ccWebServerRequest> pRequest, std::shared_ptr<ccWebServerResponse> pResponse)
 {
     //std::cout << "enter ccRESTfulChatting::list, query_string=" << pRequest->GetQueryString() << endl;
 
     switch (pRequest->GetMethod())
     {
-    case ccWebServerRequest::HttpMethod_Get:
+    case ccWebServerRequest::HttpMethod_Post:   //  Create
         {
+            static int nAutoCount = 0;
+
+            ccString    strNewID;
+            ccString    strNewName;
+
+            ccString::format(strNewID, "ID=%03d", nAutoCount);
+            ccString::format(strNewName, "Name=%03d", nAutoCount);
+
+            nAutoCount++;
+
+            if (_oSessionManager.CreateID(strNewID, strNewName) == true)
+                pResponse->Status(200, string("OK)"));
+            else
+                pResponse->Status(500, string("Server Error!"));
+        }
+        break;
+
+    case ccWebServerRequest::HttpMethod_Get:    //  Read
+        {
+            std::vector<std::string> aMembers;
+
+            _oSessionManager.GetUserList(aMembers);
+
             pResponse->Status(200, string("OK)"));
 
             Json::Value oResponseRPC;
@@ -44,11 +245,12 @@ bool ccRESTfulChatting::list(std::shared_ptr<ccWebServerRequest> pRequest, std::
             oResponseRPC["jsonrpc"] = "2.0";
             oResponseRPC["id"]      = 1;
 
-            oUserList["count"] = 2;
-            oUserList["info"][0]["id"] = "Neo";
-            oUserList["info"][0]["Name"] = "Kim Mansoo";
-            oUserList["info"][1]["id"] = "AppleHJ";
-            oUserList["info"][1]["Name"] = "Kim Hyunjun";
+            oUserList["count"] = aMembers.size();
+
+            for (int nIndex = 0; nIndex < aMembers.size(); nIndex++)
+            {
+                oUserList["info"][nIndex]["id"] = aMembers[nIndex];
+            }
 
             oResponseRPC["user_list"] = oUserList;
 
@@ -59,11 +261,26 @@ bool ccRESTfulChatting::list(std::shared_ptr<ccWebServerRequest> pRequest, std::
 
             return true;
         }
-
         break;
 
-    case ccWebServerRequest::HttpMethod_Post:
-        break;
+    case ccWebServerRequest::HttpMethod_Delete:   //  Delete
+        {
+            static int nAutoCount = 0;
+
+            ccString    strNewID;
+            ccString    strNewName;
+
+            ccString::format(strNewID, "ID=%03d", nAutoCount);
+
+            nAutoCount++;
+
+            if (_oSessionManager.DeleteID(strNewID) == true)
+                pResponse->Status(200, string("OK)"));
+            else
+                pResponse->Status(500, string("Server Error!"));
+        }
+    break;
+
     }
 
     return false;
